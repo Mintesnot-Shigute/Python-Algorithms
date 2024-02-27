c2035df86d974b58b17c786953a68a9b92514704
Mintesnot Shigute <81554903+Mintesnot-Shigute@users.noreply.github.com>
2024-02-27 15:52:22 +0300

    Update Weight.py

--- Weight.py
+++ Weight.py
@@ -1,7 +1,7 @@
 option_counts = {'A': 0, 'B': 0}  # Initialize counters for options A and B
 num_questions = 5  # Set the number of questions
 
-# Loop through each question
+
 for i in range(1, num_questions + 1):
     response = input(f"Enter response for question {i} (A/B): ").upper()  # Get user input for each question
     if response in option_counts:
