5e858e9f56f5468837989e92698cc2c64cfe035f
Mintesnot Shigute <81554903+Mintesnot-Shigute@users.noreply.github.com>
2024-03-04 11:16:56 +0300

    Update atm.py

--- ATM-Simultaneous-Python-Project-master/atm.py
+++ ATM-Simultaneous-Python-Project-master/atm.py
@@ -8,7 +8,7 @@ users = ['user', 'user2', 'user3']
 pins = ['1234', '2222', '3333']
 amounts = [1000, 2000, 3000]
 count = 0
-# while loop checks existance of the enterd username
+# while loop checks existance of the enterd
 while True:
 	user = input('\nENTER USER NAME: ')
 	user = user.lower()
