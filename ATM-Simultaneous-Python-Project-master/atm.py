01dd565f9f4239a4ee6e977c7c70fc2ffc6dd849
Mintesnot Shigute <81554903+Mintesnot-Shigute@users.noreply.github.com>
2024-03-04 11:17:56 +0300

    Update atm.py

--- ATM-Simultaneous-Python-Project-master/atm.py
+++ ATM-Simultaneous-Python-Project-master/atm.py
@@ -3,7 +3,7 @@ import getpass
 import string
 import os
 
-# creatinga lists of users, their PINs and bank 
+# creatinga lists of users, their 
 users = ['user', 'user2', 'user3']
 pins = ['1234', '2222', '3333']
 amounts = [1000, 2000, 3000]
