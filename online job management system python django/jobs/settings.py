d4d59fcacc154e77bddcaa00a593f356444de6f5
Mintesnot Shigute <81554903+Mintesnot-Shigute@users.noreply.github.com>
2024-04-06 08:53:52 +0300

    Update settings.py

--- online job management system python django/jobs/settings.py
+++ online job management system python django/jobs/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-# Build paths inside the
+# Build paths inside
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
