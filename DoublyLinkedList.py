99e873e30f21de601ccd577ef8e1dac882e8e775
Mintesnot Shigute <81554903+Mintesnot-Shigute@users.noreply.github.com>
2024-04-18 08:23:38 +0300

    Update DoublyLinkedList.py

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -44,7 +44,7 @@ class DoublyLinkedList:
                 if current_node.next_node:
                     current_node.next_node.prev_node = current_node.prev_node
 
-                return  # Node found and deleted, exit the method
+                return  # Node found and deleted, exit the meth
 
             current_node = current_node.next_node
 
