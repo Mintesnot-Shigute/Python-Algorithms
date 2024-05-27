044b02b93e95e8be282e590a23ef37c3de06c4e9
Mintesnot Shigute <81554903+Mintesnot-Shigute@users.noreply.github.com>
2024-05-27 12:13:20 +0300

    Update simple_AI.py

--- simple_AI.py
+++ simple_AI.py
@@ -1,4 +1,4 @@
-# Import necessary libraries
+# Import necessary librarie
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
