52dd12bb6fde736e16e7a8ecd884cdf31521aebf
Mintesnot Shigute <81554903+Mintesnot-Shigute@users.noreply.github.com>
2024-05-29 08:47:46 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import random
 
 password = input("Enter Your Password (Only Numbers): ")
 
-char = string.digits  # Use only digits (numbers 0-9) for the charact
+char = string.digits  # Use only digits (numbers 0-9) for the charac
 
 while True:
     var = random.choices(char, k=len(password))
