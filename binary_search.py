5714a35e27ef0fb7bdf220112e0a1cf404d72080
Mintesnot Shigute <81554903+Mintesnot-Shigute@users.noreply.github.com>
2024-05-30 11:02:50 +0300

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -9,7 +9,7 @@ def binary_search(list, target):
             low = mid + 1
         else:
             high = mid - 1
-    return -1  # If the element
+    return -1  # If the elemen
 
 
 my_list = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
