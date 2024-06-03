1c8e67cd7cf7f00e98e96d6125b1100675a01ef9
Mintesnot Shigute <81554903+Mintesnot-Shigute@users.noreply.github.com>
2024-06-03 09:57:13 +0300

    Update Weather.py

--- Weather.py
+++ Weather.py
@@ -5,7 +5,7 @@ def get_weather(api_key, city):
     params = {
         "q": city,
         "appid": api_key,
-        "units": "metric"  # You can change this 
+        "units": "metric"  # You can change this
     }
 
     response = requests.get(base_url, params=params)
