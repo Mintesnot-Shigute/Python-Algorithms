ab0af4cfc499831a975ee873e454458a5ea5058d
Mintesnot Shigute <81554903+Mintesnot-Shigute@users.noreply.github.com>
2024-06-07 08:48:44 +0300

    Update A_star_algorithm.py

--- A_star_algorithm.py
+++ A_star_algorithm.py
@@ -6,7 +6,7 @@ class Node:
         self.parent = parent
         self.g = 0  
         self.h = 0  
-        self.f = 0  #  tota
+        self.f = 0  #  tot
 
     def __lt__(self, other):
         return self.f < other.f
